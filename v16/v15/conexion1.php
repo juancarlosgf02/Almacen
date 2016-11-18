f4078127fa67dee1f8e2446532be4ea12031893e
Juan Carlos Guerrero Fernandez <juancarlos.gf02@gmail.com>
2016-11-18 14:40:35 -0500

    V16
    Nuevo inicio, csv import

---
+++ v16/v15/conexion1.php
@@ -0,0 +1,22 @@
+<?php
+/*
+$servername = "mysql.hostinger.es";
+$username = "u445983167_jcgf";
+$password = "jcgf";
+$dbname = "u445983167_bdinv";;*/
+
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bdinventarline";
+
+
+// Create connection
+$connect = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$connect) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+?> 
